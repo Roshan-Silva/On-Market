909216b99e41c7fd7e5c4c6944ffa506153cfd4b
Roshan-Silva <roshan0122silva@gmail.com>
2025-03-13 23:31:11 +0530

    Admin middleware creation

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,6 +23,8 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
+    
+
     /**
      * The application's route middleware groups.
      *
@@ -54,6 +56,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'admin' => \App\Http\Middleware\Admin::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
