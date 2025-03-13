2370efe2fe8cc4dec3838a0cbf9dffa2b1485dbb
Roshan-Silva <roshan0122silva@gmail.com>
2025-03-13 23:12:39 +0530

    Change

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title></title>
 </head>
 <body>
     <h1>ADMIN</h1>
+
+    <form method="POST" action="{{ route('logout') }}">
+     @csrf
+     <input type="submit" value="logout" />
+
+                            
+    </form>
 </body>
 </html>
