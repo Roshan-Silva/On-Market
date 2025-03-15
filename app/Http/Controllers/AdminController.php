f8850faa95fa746a320eb44d54f6d7752587e4ba
Roshan-Silva <roshan0122silva@gmail.com>
2025-03-16 02:16:17 +0530

    Sidebar modification

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,7 +10,8 @@ class AdminController extends Controller
 {
     public function view_category()
     {
-        return view('admin.category');
+        $data = Category::all();
+        return view('admin.category',compact('data'));
     }
 
     public function add_category(Request $request)
@@ -19,6 +20,33 @@ class AdminController extends Controller
        $category->Category_name = $request->category;
        $category->save();
 
+       toastr()->timeOut(10000)->closeButton()->addSuccess('Category added successfully');
+
        return redirect()->back();
+    }
+
+    public function delete_category($id)
+    {
+        $data = Category::find($id);
+        $data->delete();
+
+        toastr()->timeOut(10000)->closeButton()->addSuccess('Category deleted successfully');
+
+        return redirect()->back();
+    }
+
+    public function edit_category($id){
+        $data = Category::find($id);
+        return view('admin.edit_category',compact('data'));
+    }
+
+    public function update_category(Request $request, $id){
+        $data = Category::find($id);
+        $data->Category_name = $request->category;
+        $data->save();
+
+        toastr()->timeOut(10000)->closeButton()->addSuccess('Category updated successfully');
+
+        return redirect('/view_category');
     }
 }
