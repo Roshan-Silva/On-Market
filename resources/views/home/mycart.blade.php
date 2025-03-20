7dc2c868ce43621b513a077fa65b845434c99e27
Roshan-Silva <roshan0122silva@gmail.com>
2025-03-20 17:25:55 +0530

    Display total value of the cart

--- resources/views/home/mycart.blade.php
+++ resources/views/home/mycart.blade.php
@@ -26,6 +26,14 @@
         td{
             border: 2px solid skyblue;
         }
+        .cart_value{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 20px;
+            margin-bottom: 40px;
+            padding: 20px;
+        }
     </style>
 </head>
 
@@ -45,6 +53,11 @@
                 <th>Image</th>
                 <th>Remove</th>
             </tr>
+
+            <?php
+                $value = 0;
+            ?>
+
             @foreach ($cart as $cart)
                 
             <tr>
@@ -55,11 +68,22 @@
                 </td>
                 <td>
                     <a class="btn btn-danger" href="{{url('remove_product', $cart->id)}}">Remove</a>
+                </td>
             </tr>
+
+            <?php
+                $value = $value + $cart->product->price;
+            ?>
 
             @endforeach
         </table>
     </div>
+
+    <div class="cart_value">
+        <h3>Total Price is : ${{$value}}</h3>
+    </div>
+
+
 
 
    
