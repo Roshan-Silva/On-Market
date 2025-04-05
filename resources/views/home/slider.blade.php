4ea0d9bd17cfec23c4d08288d807ca015052097f
Roshan-Silva <roshan0122silva@gmail.com>
2025-04-05 23:53:00 +0530

    change the styles

--- resources/views/home/slider.blade.php
+++ resources/views/home/slider.blade.php
@@ -21,7 +21,7 @@
                 </div>
                 <div class="col-md-5 ">
                   <div class="img-box">
-                    <img style="width:600px" src="images/image3.jpeg" alt="" />
+                    <img style="width:600px; height:400px" src="images/HomeSlider02.jpeg" alt="" />
                   </div>
                 </div>
               </div>
